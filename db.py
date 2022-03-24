99ba46eac167e23a90b2577e11c73cc965171f52
keithb32 <77218986+keithb32@users.noreply.github.com>
2022-03-24 11:41:52 -0400

    Add module for connecting to database

---
+++ db.py
@@ -0,0 +1,34 @@
+import pymysql.cursors
+from secrets import secrets
+
+# Connect to the database
+connection = pymysql.connect(host= secrets['host'],
+                             user= secrets['user'],
+                             password= secrets['password'],
+                             database= secrets['database'],
+                             cursorclass=pymysql.cursors.DictCursor)
+
+def get_users():
+    """
+    Example function for querying data.
+    """
+
+    with connection.cursor() as cursor:
+
+        # Write query and execute it
+        query = "SELECT * FROM UserProfile;"
+        cursor.execute(query)
+
+        # Retrieve all rows from query 
+        # See https://pymysql.readthedocs.io/en/latest/modules/cursors.html for other fetch options
+        # Note - fetchAll will return results as a dictionary
+        # To read dictionary output easier, I recommend using https://beautifier.io/
+        result = cursor.fetchall()
+    
+    # Always commit after running SQL query so the changes are saved
+    connection.commit()
+
+    # Close cursor to release memory
+    cursor.close()
+
+    return result
